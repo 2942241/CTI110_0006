e7ac432b53421b8b001f4eb2d793f667c7206483
2942241 <90853440+2942241@users.noreply.github.com>
2021-09-16 11:09:16 -0400

    First upload

---
+++ P1LAB1_Epps.py
@@ -0,0 +1,16 @@
+# CTI 110
+# P1LAB1
+# Name
+# date
+
+# This is 7.12
+
+
+
+
+
+
+userNum = int(input())
+userNumSquared = userNum * userNum   # Bug here; fix it when instructed
+   
+print(userNumSquared)       # Output formatting issue here; fix it when instructed
